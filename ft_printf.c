faa0e038da9d469b7c0945aca17c6053231f4b80
Manabagy <baghdasaryanmanana@gmail.com>
2025-02-08 23:44:03 +0400

    more

--- ft_printf.c
+++ ft_printf.c
@@ -5,41 +5,48 @@ int	check_format(const char *str, args)
 	size_t	len;
 
 	len = 0;
-	if (str == 'c')
-		len += ft_putchar(va_args(args, char));
-	else if (str == 's')
-		len += ft_putstr(va_args(args, char * ));
-	else if (str == 'd')
-		len += ft_putnbr(va_args(args, int));
-	else if (str == 'i')
-		len += ft_putnbr(va_args(args, int));
-	else if (str == 'u')
-		len += ft_putunbr(va_args(args, unsigned int));
-	else if (str == 'x')
-		len +=
+	if (*str == 'c')
+		len += ft_putchar(va_arg(args, char));
+	else if (*str == 's')
+		len += ft_putstr(va_arg(args, char * ));
+	else if (*str == 'd')
+		len += ft_putnbr(va_arg(args, int));
+	else if (*str == 'i')
+		len += ft_putnbr(va_arg(args, int));
+	else if (*str == 'u')
+		len += ft_putunbr(va_arg(args, unsigned int));
+	else if (*str == 'x')
+		len += ft_puthex(va_arg(args, unsigned int), 0);
+	else if (*str == 'X')
+		len += ft_puthex(va_arg(args, unsigned int), 1);
+	else if (*str == '%')
+		len += byte_putchar('%');
 
 }
 
 int	ft_printf(const char *format, ...)
 {
 	va_list args;
-	int		a;
-	va_list *res;
+	int		count;
 
 	va_start(args, format);
 
-	a = 0;
+	const = 0;
 	while (format[a] != '\0')
 	{
 		if (format[a] == '%')
 		{
-			if (format[a + 1] == 'd')
-			{
-				va_args(args, int);
-				va_copy(&res, &format);
-			}
+			str++;
+			count += check_format(str++, args);
 		}
-		write(1, &format[a], 1);
+		else
+		{
+			count += ft_putchar(format);
+			str++;
+		}
 		a++;
 	}
+	va_end(args);
+	return (count);
+
 }
--- functions.c
+++ functions.c
@@ -3,7 +3,7 @@
 size_t	ft_putchar(char c)
 {
 	write(1, &c, 1);
-	return 0;
+	return (1);
 }
 
 size_t ft_putunbr(unsigned int nbr)
@@ -19,3 +19,21 @@ size_t ft_putunbr(unsigned int nbr)
 	return (len);
 }
 
+int	ft_puthex(unsigned int n, int uppercase)
+{
+	char *base;
+	int len;
+
+	len = 0;
+	if (uppercase)
+		base = "0123456789ABCDEF";
+	else
+		base = "0123456789abcdef";
+
+	if (n >= 16)
+		len += ft_puthex(n / 16, uppercase);
+	ft_putchar(base[n % 16]);
+	len++;
+
+	return (len);
+}
